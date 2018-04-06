ea98f2abe1ac09fc15e1ed91cd2cc08387dde840
20123161 <maxime.tasset@student.ulg.ac.be>
2018-04-06 23:00:10 +0200

    On a fait quelque chose ;)

--- iterativeA3c.py
+++ iterativeA3c.py
@@ -5,17 +5,48 @@ Created on Fri Apr  6 17:38:41 2018
 @author: Sarah
 """
 import pacman
-from reinfpacman import Reinfpacman
-from reinfghost import Reinfghost
+from reinfagent import ReinfAgent
+import layout
 
-def iterativeA3c(nb_ghost=3):
-    
+
+def iterativeA3c(nb_ghosts=3,nb_training=1,display_mode='graphics'):
+
+    # Choose a display format
+    if display_mode == 'quiet':
+        import textDisplay
+        display = textDisplay.NullGraphics()
+    elif display_mode == 'text':
+        import textDisplay
+        textDisplay.SLEEP_TIME = 0.1
+        display = textDisplay.PacmanGraphics()
+    else:
+        import graphicsDisplay
+        display = graphicsDisplay.PacmanGraphics(1.0, frameTime=0.1)
+
     agents = list()
-    
-    agent.append(Reinfpacman()) 
-    
+
+    agents.append(ReinfAgent())
+
     for i in range(1,nb_ghosts+1):
-        agent.append(Reinfghost(i))
-    
-    while nb_it<100 or consec_wins<50:
-        pass
+        agents.append(ReinfAgent(i))
+
+    layout_instance = layout.getLayout('mediumClassic')
+
+    nb_it = 0
+    consec_wins = 0
+    winner = 0
+
+    while nb_it<100 or abs(consec_wins)<50:
+
+      for i in range(nb_ghosts+1):
+        agents[i].startLearning()
+        games = pacman.runGames(layout_instance,agents[0],agents[1:],display,nb_training,False,timeout=30)
+        #compute how many consecutive win ghosts or pacman have
+        for game in games:
+          if game.state.isWin():
+            consec_wins = max(1,consec_wins+1)
+          else:
+            consec_wins = min(-1,consec_wins-1)
+        agents[i].learnFromPast()
+        agents[i].stopLearning()
+        nb_it += 1
--- reinfagent.py
+++ reinfagent.py
@@ -8,23 +8,25 @@ from pacman import Directions
 from game import Agent
 from ghostAgents import GhostAgent
 import util
-import sklearn
+from sklearn.neural_network import MLPRegressor
+import numpy as np
+
+
 
 class ReinfAgent(GhostAgent,Agent):
 
-    def __init__(self, index=0, time_eater=40, g_pattern=-1, 
-                 prob_attack=1, prob_scaredFlee=1):
+    def __init__(self, index=0,epsilon=0.1):
 
         self.lastMove = Directions.STOP
         self.index = index
-        self.keys = []
-        self.prob_attack = prob_attack
-        self.prob_scaredFlee = prob_scaredFlee
-        self.learning_algo = sklearn.MLPRegressor()
-    
+
+        self.learning_algo = None
+        self.learn = False
+        self.epsilon = epsilon
+
     def getDistribution(self, state):
         # ghost function
-        
+
         dist = util.Counter()
         legalActions = state.getLegalActions(self.index)
         for a in legalActions:
@@ -32,15 +34,54 @@ class ReinfAgent(GhostAgent,Agent):
         dist[self.getAction(state)] = 1
         dist.normalize()
         return dist
-    
+
     def getAction(self, state):
         # pacman function
+
         legalActions = state.getLegalActions(self.index)
-        move = legalActions[1]
+
+        #if we don't have learn yet, make random move + epsilon greedy
+        if self.learning_algo is None or np.random.uniform() <= self.epsilon:
+            move = legalActions[np.random.randint(0,len(legalActions))]
+        else:
+            move = legalActions[np.argmax(self.learning_algo.predict(np.array([(getDataState(state),a) for a in legalActions])))]
+
+        if self.learn:
+            self._saveOneStepTransistion(state,move)
+
         return move
-    
+
     def startLearning(self):
-        learn = True
-        
+        self.learn = True
+        self.one_step_transistion = []
+
     def stopLearning(self):
-        learn = False
+        self.learn = False
+
+    def learnFromPast(self):
+        if len(self.one_step_transistion):
+            if self.learning_algo is None:
+#              self.learning_algo = MLPRegressor()
+              #TODO: faire l'algo...
+              pass
+
+
+    def _saveOneStepTransistion(self,state,move):
+
+        nextState = state.generateSuccessor(self.index,move)
+
+        if self.index:
+            #ghost reward
+            reward = -util.manhattanDistance(nextState.getGhostPosition(self.index),
+                                          nextState.getPacmanPosition()) - \
+                     1000 * nextState.isWin() + 10000 * nextState.isLose()
+        else:
+            #pacman reward
+            reward = -1 + 1000 * nextState.isWin() - 100000 * nextState.isLose()
+
+        state_data = getDataState(state)
+        nextState_data = getDataState(nextState)
+        self.one_step_transistion.append((state_data,move,reward,nextState_data))
+
+def getDataState(state):
+  return (state.data.agentStates[:],state.getFood().copy(),state.getCapsules().copy())
